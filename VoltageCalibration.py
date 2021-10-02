612e523dac35843ce092db50e2755d5942ac7507
rainman <nope@nope.com>
2021-10-02 12:33:26 +0200

    Added command line options to voltage calibration Voltage Calibration now creates entire list of channels

--- VoltageCalibration.py
+++ VoltageCalibration.py
@@ -1,8 +1,14 @@
 from ArduVoltmeter import ArduVoltmeter
 import serial
 import json
+from optparse import OptionParser
 
-voltMeter = ArduVoltmeter(channelList=[0], port=serial.Serial('COM4', 9600))
+opt = OptionParser()
+opt.add_option('-p', '--port', dest='port', help='Serial port name', default='/dev/ttyAMC0')
+opt.add_option('-b', '--baud', dest='baud', help='Serial port baud rate', default=9600, type='int')
+options, args = opt.parse_args()
+
+voltMeter = ArduVoltmeter(channelList=[0, 1, 2, 3, 4, 5], port=serial.Serial(options.port, options.baud))
 
 voltMeter.start()
 voltMeter.waitReady()
@@ -18,7 +24,7 @@ while cont:
     break
 
   volts.append(float(inp))
-  cnt = voltMeter.readValuesWait()[1][0]
+  cnt = voltMeter.readValuesWait()[1]
   print('Counts: %s' % cnt)
   counts.append(cnt)
 
